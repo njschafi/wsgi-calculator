f0706906116d17d699c3ce0112b7afd5da08d96f
njschafi <njschafi@gmail.com>
2019-07-22 20:13:07 -0700

    completed lesson04 assignment. Passes all tests in tests.py

--- calculator.py
+++ calculator.py
@@ -1,84 +1,116 @@
 """
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
+Neima Schafi - Lesson04 Assignment
 """
-
+from functools import reduce
+import operator
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    sum_all = "0"
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    try:
+        total = sum(map(int, args))
+        sum_all = str(total)
+    except (ValueError, TypeError , IndexError):
+        print("Unable to calculate sum due to possible non-integer or no integers provided")
+    return sum_all
 
-    return sum
+def subtract(*args):
+    """ Returns a STRING with the results of subtracting the arguments """
+    results = "0"
+    try:
+        args = list(map(int, args))
+        total = args[0] - sum(args[1:])
+        results = str(total)
+    except (ValueError, TypeError):
+        print("Unable to calculate sum due to possible non-integer")
+    return results
 
-# TODO: Add functions for handling more arithmetic operations.
+def multiply(*args):
+    """ Returns a STRING with the result of multiplying the arguments """
+    results = "0"
+    try:
+        args = list(map(int, args))
+        total = reduce(operator.mul, args, 1)
+        results = str(total)
+    except (ValueError, TypeError , IndexError):
+        print("Unable to calculate sum due to possible non-integer or no integers provided")
+    return results
+
+def divide(*args):
+    """ Returns a STRING with the result of dividing the arguments """
+    results = "0"
+
+    try:
+        args = list(map(int, args))
+        total = reduce(operator.truediv, args)
+        results = str(total)
+    except (ValueError, TypeError, IndexError):
+        print("Unable to calculate sum due to possible non-integer or no integers provided")
+    except ZeroDivisionError as e:
+        return "Error - You divided by 0, try again."
+    return results
+
+def homepage():
+    """ Home page for calculations. Home page explains how to perfrom calculations """
+    # I should also be able to see a home page (http://localhost:8080/)
+    # that explains how to perform calculations.
+    body = """<html>
+            <head>
+            <title>WSGI calculator</title>
+            </head>
+            <body>
+            <p> Welcome to the WSGI calculator page!!</p>
+            <p> Using the WSGI calculator you can add, subtract, multiply, or divide two (or more) numbers.</p>
+            <p>
+            You can use the calculator by appending any of the mentioned actions (add, subtract, multiply, or divide two numbers)
+            to http://localhost:8080/ as such: http://localhost:8080/multiply/3/5 => 15
+            </p>
+            </html>"""
+    return body
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    funcs = {'': homepage, 'add': add, 'subtract': subtract,
+            'multiply': multiply, 'divide': divide,}
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
-
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
+    start_response(status, headers)
+    return ["<h1>No Progress Yet</h1>".encode('utf8')]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
